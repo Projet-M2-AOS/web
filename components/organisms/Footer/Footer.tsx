07dd9902d64efa6c4793c4bceaa21da5f903c792
kilianbraquin <kbraquin@indiebaie.com>
2022-01-03 19:47:42 +0100

    feat: add names in Footer

--- components/organisms/Footer/Footer.tsx
+++ components/organisms/Footer/Footer.tsx
@@ -3,11 +3,13 @@ import type { FC } from "react";
 
 export const Footer: FC = () => {
   return (
-    <footer className="flex flex-col items-center justify-center h-32 space-y-1 text-white bg-neutral-700">
+    <footer className="flex flex-col items-center justify-center h-32 px-4 space-y-1 text-white bg-neutral-700">
       <div>
         <Logo className="fill-primary-300" />
       </div>
-      <div>© AOS Team - 2021</div>
+      <div className="font-semibold">
+        Yoann LEBRETON - Kilian BRAQUIN - Tom FLENNER
+      </div>
     </footer>
   );
 };
